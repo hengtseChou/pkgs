c6808da1263daae5398cde71cafe473e43d3a07e
hengtseChou <hankthedev@gmail.com>
2025-01-11 14:46:45 +0800

    add auto update procedure

--- update.sh
+++ update.sh
@@ -4,3 +4,11 @@ pacman -Qqe > explicit.tmp
 grep -Fxv -f base explicit.tmp > extra.tmp
 grep -Fxv -f aur extra.tmp > extra
 rm *.tmp
+if [[ -n $(git diff) ]]; then
+  if [[ $1 == "--auto" ]]; then
+    git add .
+    git commit -m 'auto update: pkgs changed'
+    git push
+  fi
+fi
+
